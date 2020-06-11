4d53eee92e7d4a60899d66a440dfee34f45eb336
Marek Miklaszewski <mar.mik1@wp.pl>
2020-06-11 23:00:14 +0200

    Create firebase helper func to get product objects

--- src/helpers/firabaseFunctions.js
+++ src/helpers/firabaseFunctions.js
@@ -17,3 +17,14 @@ export const fetchStorageURL = async ref => {
   const url = await storage.refFromURL(ref).getDownloadURL();
   return url;
 };
+
+export const convertToProductObjectsFrom = async collection => {
+  const items = await Promise.all(
+    collection.docs.map(async doc => {
+      const { name, price, imgRef, modelRef, description } = doc.data();
+      const img = await fetchStorageURL(imgRef);
+      return { name, price, img, modelRef, description, id: doc.id };
+    })
+  );
+  return items;
+};
