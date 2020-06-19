5e88e24cd273d18195db34500305d00588e54fc4
Marek Miklaszewski <mar.mik1@wp.pl>
2020-06-19 17:19:45 +0200

    Updates

---
+++ precache-manifest.f2839d5556450b7210527d1519d48c60.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b794b237833f76cfeb7dd72f84bd449e",
+    "url": "/3shoop/index.html"
+  },
+  {
+    "revision": "4134a2f08dd11b2f5b8a",
+    "url": "/3shoop/static/css/2.9e6647f0.chunk.css"
+  },
+  {
+    "revision": "4134a2f08dd11b2f5b8a",
+    "url": "/3shoop/static/js/2.31199e65.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/3shoop/static/js/2.31199e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89919c59a892366b1afd",
+    "url": "/3shoop/static/js/main.3978e521.chunk.js"
+  },
+  {
+    "revision": "9d3cafdef8389e254372",
+    "url": "/3shoop/static/js/runtime-main.63b23f18.js"
+  },
+  {
+    "revision": "b16848209b48b1ce4e95aed6ccb91cf3",
+    "url": "/3shoop/static/media/Visa.b1684820.svg"
+  },
+  {
+    "revision": "2ee2f8467585eb9c06d314a127be1046",
+    "url": "/3shoop/static/media/hero.2ee2f846.jpg"
+  },
+  {
+    "revision": "543bc93b2e32281bad1ede21bb3afbdd",
+    "url": "/3shoop/static/media/mc_symbol.543bc93b.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/3shoop/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/3shoop/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/3shoop/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/3shoop/static/media/slick.f97e3bbf.svg"
+  }
+]);
